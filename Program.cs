625e4500d7bf5b396843cf1b9929dc67846bb785
Danilo Costa <dandan_costa2222@outlook.com>
2025-04-12 13:06:21 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿double a, b, c, x1 , x2 , delta;
+
+Console.Write("Insira o coeficiente a: ");
+a = double.Parse(Console.ReadLine()!);
+
+Console.Write("Insira o coeficiente b: ");
+b = double.Parse(Console.ReadLine()!);
+
+Console.Write("Insira o coeficiente c: ");
+c = double.Parse(Console.ReadLine()!);
+
+if (a == 0){
+    Console.WriteLine("Os valores tem que ser diferentes de 0 para que seja uma equação de 2º  grau");
+}
+else{
+
+}
+delta = b * b - 4 * a * c;
+
+if (delta < 0){
+    Console.WriteLine("Essa equação não possui raizes reais. Ela possui raizes complexas ");
+}
+else if(delta > 0){
+    x1 = (-b + Math.Sqrt(delta) / 2 * a);  
+    x2 = (-b - Math.Sqrt(delta) / 2 * a);
+    Console.WriteLine($"O valor de x1 é {x1}");
+    Console.WriteLine($"O valor de x2 é {x2}");   
+}
+else if (delta == 0){
+    x1 = -b / 2 * a;
+    Console.WriteLine($"O valor de x1 é {x1}");
+}
+
+
